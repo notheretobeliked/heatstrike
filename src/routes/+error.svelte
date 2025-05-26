a22c1ce2784180d9f764c8b5848bd6c76be93b1b
Erik Hartin <erik@nhtbl.studio>
2025-05-26 23:38:11 +0100

    Better error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,11 +2,11 @@
 	import { page } from '$app/state';
 </script>
 
-<div class="mt-40 min-h-[calc(100vh-60vw)]">
-<h1 class="text-[10rem] leading-42">{page.status}</h1>
+<div class="mt-40 min-h-screen bg-caution max-w-screen-lg mx-auto">
+<h1 class="text-[10rem] leading-42 text-extremedanger">{page.status}</h1>
 {#if (page.status === 404) }
-<p class="text-base mb-3">You seem to be looking for a page that doesn't exist</p>
-<p class="text-base mb-3">Go back to the <a href="/">homepage</a> to learn more about Citizen's Arrest Network.</p>
+<p class="text-base mb-3">Apologies, but you seem to be as lost as the government climate policies.</p>
+<p class="text-base mb-3">Go back to the <a href="/">homepage</a> to learn more about Heat Strike.</p>
 {:else}
 <p class="text-base">{page.error.message}</p>
 {/if}
